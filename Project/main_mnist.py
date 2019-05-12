a25cd3560f66008ac3828e4782a9f4c82d320301
xususan <susan.xu97@gmail.com>
2019-05-11 22:06:06 -0400

    jk change back to info

--- Project/main_mnist.py
+++ Project/main_mnist.py
@@ -172,7 +172,7 @@ def load_mnist():
 
 
 def main(unused_argv):
-  tf.logging.set_verbosity(tf.logging.ERROR)
+  tf.logging.set_verbosity(tf.logging.INFO)
   if FLAGS.dpsgd and FLAGS.batch_size % FLAGS.microbatches != 0:
     raise ValueError('Number of microbatches should divide evenly batch_size')
 
