cb8b03b1cd4874bdde319c4df33b54756b7b3303
xususan <susan.xu97@gmail.com>
2019-05-12 16:26:31 -0400

    name

--- Project/main.py
+++ Project/main.py
@@ -348,7 +348,7 @@ def ff_model_fn(features, labels, mode):
 	input_layer = tf.reshape(features['x'], [-1, 32, 32, 3])
 	y = layers.Flatten().apply(input_layer)
 	y = layers.Dense(50).apply(y)
-	y = layers.Dense(num_classes).apply(y)
+	logits= layers.Dense(num_classes).apply(y)
 
 
 
