89b7064f863d7148096285b9cf33a1be61afbfc1
xususan <susan.xu97@gmail.com>
2019-05-12 16:46:10 -0400

    rereRE factor

--- Project/main.py
+++ Project/main.py
@@ -165,86 +165,6 @@ def generate_estimator_spec(logits, features, labels, mode):
 											loss=scalar_loss,
 											eval_metric_ops=eval_metric_ops)
 
-def model_function_from_model(model, features, labels, mode):
-	logits = model(features['x'])
-
-	# Calculate loss as a vector (to support microbatches in DP-SGD).
-	vector_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels= labels,logits=logits)
-	# Define mean of loss across minibatch (for reporting through tf.Estimator).
-	scalar_loss = tf.reduce_mean(vector_loss)
-
-	# Configure the training op (for TRAIN mode).
-	if mode == tf.estimator.ModeKeys.TRAIN:
-
-		if FLAGS.dp:
-			ledger = privacy_ledger.PrivacyLedger(
-				population_size=60000,
-				selection_probability=(FLAGS.batch_size / 60000))
-
-			# Use DP version of GradientDescentOptimizer. Other optimizers are
-			# available in dp_optimizer. Most optimizers inheriting from
-			# tf.train.Optimizer should be wrappable in differentially private
-			# counterparts by calling dp_optimizer.optimizer_from_args().
-			if FLAGS.optim == 'sgd':
-				optimizer_func = dp_optimizer.DPGradientDescentGaussianOptimizer
-			elif FLAGS.optim == 'adam':
-				optimizer_func = dp_optimizer.DPAdamGaussianOptimizer
-			elif FLAGS.optim == 'adagrad':
-				optimizer_func = dp_optimizer.DPAdagradGaussianOptimizer
-			else:
-				raise ValueError("optimizer function not supported")
-
-
-
-			optimizer = optimizer_func(
-				l2_norm_clip=FLAGS.l2_norm_clip,
-				noise_multiplier=FLAGS.noise_multiplier,
-				num_microbatches=FLAGS.microbatches,
-				ledger=ledger,
-				learning_rate=FLAGS.learning_rate)
-			training_hooks = [
-				EpsilonPrintingTrainingHook(ledger)
-			]
-			opt_loss = vector_loss
-		else:
-			if FLAGS.optim == 'sgd':
-				optimizer_func = GradientDescentOptimizer
-			elif FLAGS.optim == 'adam':
-				optimizer_func = AdamOptimizer
-			elif FLAGS.optim == 'adagrad':
-				optimizer_func = AdagradOptimizer
-			else:
-				raise ValueError("optimizer function not supported")
-			optimizer = GradientDescentOptimizer(learning_rate=FLAGS.learning_rate)
-			training_hooks = []
-			opt_loss = scalar_loss
-		global_step = tf.train.get_global_step()
-		train_op = optimizer.minimize(loss=opt_loss, global_step=global_step)
-		# In the following, we pass the mean of the loss (scalar_loss) rather than
-		# the vector_loss because tf.estimator requires a scalar loss. This is only
-		# used for evaluation and debugging by tf.estimator. The actual loss being
-		# minimized is opt_loss defined above and passed to optimizer.minimize().
-		return tf.estimator.EstimatorSpec(mode=mode,
-											loss=scalar_loss,
-											train_op=train_op,
-											training_hooks=training_hooks)
-
-	# Add evaluation metrics (for EVAL mode).
-	elif mode == tf.estimator.ModeKeys.EVAL:
-		eval_metric_ops = {
-			'accuracy':
-				tf.metrics.accuracy(
-					labels=labels,
-					predictions=tf.argmax(input=logits, axis=1)),
-			'crossentropy':
-				tf.metrics.mean(
-					scalar_loss
-					)
-		}
-
-		return tf.estimator.EstimatorSpec(mode=mode,
-											loss=scalar_loss,
-											eval_metric_ops=eval_metric_ops)
 
 def lr_nonpca_model_fn(features, labels, mode):
 	""" Model function for logistic regression."""
@@ -260,65 +180,7 @@ def lr_nonpca_model_fn(features, labels, mode):
 	logits = layers.Dense(
 		num_classes).apply(y)
 
-	# Calculate loss as a vector (to support microbatches in DP-SGD).
-	vector_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
-			labels=labels, logits=logits)
-	# Define mean of loss across minibatch (for reporting through tf.Estimator).
-	scalar_loss = tf.reduce_mean(vector_loss)
-
-	# Configure the training op (for TRAIN mode).
-	if mode == tf.estimator.ModeKeys.TRAIN:
-
-		if FLAGS.dp:
-			ledger = privacy_ledger.PrivacyLedger(
-					population_size=60000,
-					selection_probability=(FLAGS.batch_size / 60000))
-
-			# Use DP version of GradientDescentOptimizer. Other optimizers are
-			# available in dp_optimizer. Most optimizers inheriting from
-			# tf.train.Optimizer should be wrappable in differentially private
-			# counterparts by calling dp_optimizer.optimizer_from_args().
-			optimizer = dp_optimizer.DPGradientDescentGaussianOptimizer(
-					l2_norm_clip=FLAGS.l2_norm_clip,
-					noise_multiplier=FLAGS.noise_multiplier,
-					num_microbatches=FLAGS.microbatches,
-					ledger=ledger,
-					learning_rate=FLAGS.learning_rate)
-			training_hooks = [
-					EpsilonPrintingTrainingHook(ledger)
-			]
-			opt_loss = vector_loss
-		else:
-			optimizer = GradientDescentOptimizer(learning_rate=FLAGS.learning_rate)
-			training_hooks = []
-			opt_loss = scalar_loss
-		global_step = tf.train.get_global_step()
-		train_op = optimizer.minimize(loss=opt_loss, global_step=global_step)
-		# In the following, we pass the mean of the loss (scalar_loss) rather than
-		# the vector_loss because tf.estimator requires a scalar loss. This is only
-		# used for evaluation and debugging by tf.estimator. The actual loss being
-		# minimized is opt_loss defined above and passed to optimizer.minimize().
-		return tf.estimator.EstimatorSpec(mode=mode,
-											loss=scalar_loss,
-											train_op=train_op,
-											training_hooks=training_hooks)
-
-	# Add evaluation metrics (for EVAL mode).
-	elif mode == tf.estimator.ModeKeys.EVAL:
-		eval_metric_ops = {
-				'accuracy':
-						tf.metrics.accuracy(
-								labels=labels,
-								predictions=tf.argmax(input=logits, axis=1))
-		}
-
-		return tf.estimator.EstimatorSpec(mode=mode,
-			loss=scalar_loss,
-			eval_metric_ops=eval_metric_ops)
-
-
-
-	return model_function_from_model(lr_model, features, labels, mode)
+	return generate_estimator_spec(logits, features, labels, mode)
 
 def lr_model_fn(features, labels, mode):
 	""" Model function for logistic regression."""
@@ -350,12 +212,6 @@ def ff_model_fn(features, labels, mode):
 	y = layers.Flatten().apply(input_layer)
 	y = layers.Dense(50, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(C)).apply(y)
 	logits= layers.Dense(num_classes, kernel_regularizer=tf.keras.regularizers.l2(C)).apply(y)
-
-
-	# y = layers.Dense(
-	# 	50, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(C)
-	# 	).apply(y)
-	# logits = layers.Dense(num_classes, kernel_regularizer=tf.keras.regularizers.l2(C)).apply(y)
 
 
 	return generate_estimator_spec(logits, features, labels, mode)
@@ -411,7 +267,9 @@ def cnn_model_fn(features, labels, mode):
 		# y = layers.Dropout(0.5).apply(y)
 		logits = layers.Dense(num_classes).apply(y)
 		return logits
-	return model_function_from_model(cnn_model, features, labels, mode)
+
+	logits = cnn_model(features['x'])
+	return generate_estimator_spec(logits, features, labels, mode)
 
 
 def load_cifar_pca():
